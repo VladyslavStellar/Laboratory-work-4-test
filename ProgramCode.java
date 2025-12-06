4e717dbf10e6175fc359732f2e5bd9ecf9afbe79
VladyslavStellar <eugene.stellar.tech@gmail.com>
2025-12-06 14:35:33 +0100

    Update ProgramCode.java

--- ProgramCode.java
+++ ProgramCode.java
@@ -1,5 +1,14 @@
-class ProgramCode {
-  public static void main(String args[]) {
+/**
+ * Main class for EcoPoet Finder application.
+ */
+public class ProgramCodeModified {
+
+  /**
+   * Main entry point.
+   *
+   * @param args Command line arguments.
+   */
+  public static void main(String[] args) {
     System.out.println("EcoPoet Finder Product");
   }
 }
