f91493bd81d0a5c0698e80667b8103783dabd9e2
VladyslavStellar <eugene.stellar.tech@gmail.com>
2025-12-06 15:30:32 +0100

    Update ProgramCode.java

--- ProgramCode.java
+++ ProgramCode.java
@@ -1,5 +1,22 @@
-class ProgramCode {
-public static void main(String args[]){
-System.out.println("EcoPoet Finder Product");
-}
+/**
+ * Main class for EcoPoet Finder application.
+ */
+public final class ProgramCode {
+
+  /**
+   * Private constructor to prevent instantiation.
+   * Utility classes should not have a public or default constructor.
+   */
+  private ProgramCode() {
+    // Prevent instantiation
+  }
+
+  /**
+   * Main entry point.
+   *
+   * @param args Command line arguments.
+   */
+  public static void main(final String[] args) {
+    System.out.println("EcoPoet Finder Product");
+  }
 }
